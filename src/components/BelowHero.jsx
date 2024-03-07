cfdd2de84b6c878dafc2a0adc15d1d12b1a55049
Oubaid <oubaidm@live.com>
2024-03-07 21:06:11 +0100

    finished below hero section

--- src/components/BelowHero.jsx
+++ src/components/BelowHero.jsx
@@ -2,15 +2,17 @@ import interactive from '../../images/mobile/image-interactive.jpg';
 
 export default function BelowHero() {
   return (
-    <section>
+    <section className='content'>
       <img src={interactive} alt='' />
-      <h2>The leader in interactive VR</h2>
-      <p>
-        Founded in 2011, Loopstudios has been producing world-class virtual
-        reality projects for some of the best companies around the globe. Our
-        award-winning creations have transformed businesses through digital
-        experiences that bind to their brand.
-      </p>
+      <div className='content__text'>
+        <h2>The leader in interactive VR</h2>
+        <p>
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
     </section>
   );
 }
